29f7a1bb4b7daa611109d4c6c1025a3da10a13f2
Kalvakuri,Lakshmi Seshu <s533707@nw41493.nwmissouri.edu>
2019-01-31 10:18:46 -0600

    Initial Commit

---
+++ hello Xcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello Xcode
+//
+//  Created by Kalvakuri,Lakshmi Seshu on 1/31/19.
+//  Copyright © 2019 northwest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
